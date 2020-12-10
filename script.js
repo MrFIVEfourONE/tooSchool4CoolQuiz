fd7b91141ae769c5ab813e801a433af898779a3b
MrFIVEfourONE <choffman04@gmail.com>
2020-12-09 22:46:15 -0800

    went over what i had with a peer and decided to go for a more dynamicaly updated through JS approch so alot has changed

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
-// this is the timer function
-import react from 'react';
 
+// const question = document.getElementById("question");
+// const choice = Array.from(document.getElementsByClassName("choice-text"));
+
+// let currentQuestion = {};
+// let acceptingAnswers = true;
+
+
+
+// importing El form DOM
+const questionTitleEl = document.querySelector("#question-title")
+
+const choiceContainerEl = document.querySelector("#choice-container")
+
+const rightOrWrongEl = document.querySelector("#right-wrong")
+
+// declare VAR
+let questionIndex = 0;
+
+// array of objects
+const questions = [
+{
+    title: "what is 1 + 1",
+    choices: [1,2,3,4],
+    answer: 2
+},
+{
+    title: "what is 2 + 2",
+    choices: [1,2,3,4],
+    answer: 4
+}
+]
+
+function generateQuestion() {
+    const currentQuestion = questions[questionIndex];
+    questionTitleEl.textContent = currentQuestion.title
+    currentQuestion.choices.forEach(function(value) {
+        let button = document.createElement("button");
+        button.setAttribute("class","btnChoice")
+        button.setAttribute("value", value)
+        button.textContent = value;
+        button.onclick = buttonClicked
+        choiceContainerEl.appendChild(button)
+
+    })
+}
+
+function buttonClicked() {
+
+}
+
+function start() {
+    generateQuestion()
+}
+
+start()
+
+
+// generate Q function
+// validate
+
+// timer funtion
+
+// function to decrease time if wrong answer
+
+// end FNCT
