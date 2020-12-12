a0e5fed3b682580377c5e786677dff8613dffabf
MrFIVEfourONE <choffman04@gmail.com>
2020-12-12 12:01:43 -0800

    have var declared and the array for questions set up

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 
 const question = document.getElementById( 'question' );
 const choices = Array.from( document.getElementsByClassName( 'choice-text' ) );
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [ {
+    question: choice1: choice2: choice3: choice4: answer:
+}, ]
