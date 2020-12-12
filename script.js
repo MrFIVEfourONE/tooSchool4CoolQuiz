830043ee953476c41a41315188836bc83c9e8bf2
MrFIVEfourONE <choffman04@gmail.com>
2020-12-12 12:38:31 -0800

    got the questions filled in to the array

--- script.js
+++ script.js
@@ -10,5 +10,48 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [ {
-    question: choice1: choice2: choice3: choice4: answer:
-}, ]
+        question: "JavaScript is a ___ - side programming language.",
+        choice1: "server",
+        choice2: "client",
+        choice3: "both",
+        choice4: "None",
+        answer: 3,
+
+    },
+    {
+        question: "Which of the following will write the message“ Hello World!” in an alert box ?",
+        choice1: "alertBox("
+        Hello World!");",
+        choice2: "alert("
+        Hello World!");",
+        choice3: "msgAlert("
+        Hello World!");",
+        choice4: "alert("
+        Hello World!");",
+        answer: 4,
+    },
+    {
+        question: "How do you find the minimum of x and y using JavaScript?",
+        choice1: "min(x,y);",
+        choice2: "Math.min(x,y)",
+        choice3: "Math.min(xy)",
+        choice4: "min(xy)",
+        answer: 2,
+    },
+    {
+        question: "Which JavaScript label catches all the values, except for the ones specified?",
+        choice1: "catch",
+        choice2: "label",
+        choice3: "try",
+        choice4: "default",
+        answer: 4,
+    },
+    {
+        question: "What will the code return? Boolean(3<7)",
+        choice1: "true",
+        choice2: "false",
+        choice3: "NaN",
+        choice4: "SyntaxError",
+        answer: 1,
+    },
+];
