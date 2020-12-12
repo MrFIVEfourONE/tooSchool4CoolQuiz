30ecf18fafbe2a71bed587b4449c0ae6f3322d99
MrFIVEfourONE <choffman04@gmail.com>
2020-12-12 13:32:32 -0800

    added a start page to use a function to start game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // dynamicaly run questions through a loop with an array and post to elements in html
 
-const question = document.getElementById( 'question' );
-const choices = Array.from( document.getElementsByClassName( 'choice-text' ) );
+const question = document.getElementById( "question" );
+const choices = Array.from( document.getElementsByClassName( "choice-text" ) );
 
 let currentQuestion = {};
 let acceptingAnswers = false;
