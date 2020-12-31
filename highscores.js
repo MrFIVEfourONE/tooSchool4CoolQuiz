7a23d126d5e30b0b2b1b2b633329685688a21ec1
MrFIVEfourONE <choffman04@gmail.com>
2020-12-31 02:07:39 -0800

    im done, i fixed some links and finished the css as much as i care to for a fake quiz that has been nothing but a pain in my ....life for the past 3weeks.... wish i had the ability to half ass things.

--- highscores.js
+++ highscores.js
@@ -1,14 +1,16 @@
-const submit = getElementById( "sumbit" );
-const initials = document.getElementById( "Initials" ).value;
+init();
 
-
-
-
-
-submit.addEventListener( "click", function () {
-            var initials;
-            var finalScore = {
-                initials: initials,
-                score: timeRemaining
-            };
-            console.log( finalScore );
+function init() {
+    const submit = document.getElementById( "submit" );
+    submit.addEventListener( "click", function () {
+        const initials = document.getElementById( "initials" ).value;
+        console.log( initials );
+        const timeRemaining = localStorage.getItem( 'timeRemaining' )
+        console.log( timeRemaining );
+        document.getElementById( "highName" ).style.display = "none"; //hide
+        // document.getElementById( "highName" ).classList.add( "hide" )
+        document.getElementById( "highscoreList" ).style.display = "block"; //show
+        // document.getElementById( "highscoreList" ).classList.remove( "hide" )
+        document.getElementById( "displayHide" ).textContent = "  " + initials + "  did it in:  " + timeRemaining
+    } );
+}
