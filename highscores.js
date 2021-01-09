4972129e43553e8be6a4e064edf3b3b010fb90bf
Maryus Martsyalis <martsyalis@gmail.com>
2021-01-09 10:01:19 -0800

    refactored high scores to keep track of past scores

--- highscores.js
+++ highscores.js
@@ -3,14 +3,31 @@ init();
 function init() {
     const submit = document.getElementById( "submit" );
     submit.addEventListener( "click", function () {
+        let tooSchoolForCoolScores;
+        if(localStorage.getItem('tooSchoolForCoolScores')) {
+            tooSchoolForCoolScores = JSON.parse(localStorage.getItem('tooSchoolForCoolScores'));
+        } else {
+            tooSchoolForCoolScores = [];
+        }
+        const lastScore = localStorage.getItem( 'latest-score' ) 
         const initials = document.getElementById( "initials" ).value;
+        const userScore = { [initials]: lastScore}
+        tooSchoolForCoolScores.push(userScore);
+        localStorage.setItem('tooSchoolForCoolScores', JSON.stringify(tooSchoolForCoolScores));
         console.log( initials );
         const timeRemaining = localStorage.getItem( 'timeRemaining' )
+
         console.log( timeRemaining );
         document.getElementById( "highName" ).style.display = "none"; //hide
         // document.getElementById( "highName" ).classList.add( "hide" )
         document.getElementById( "highScoreList" ).style.display = "block"; //show
         // document.getElementById( "highscoreList" ).classList.remove( "hide" )
-        document.getElementById( "displayHide" ).textContent = "  " + initials + "  did it in:  " + timeRemaining
+        tooSchoolForCoolScores.forEach((record) => {
+            Object.keys(record).forEach(key => {
+                document.getElementById( "displayHide" ).textContent += " \n " + key + "'s score was:  " + record[key] +' \n '
+            })
+
+        })
+
     } );
 }
